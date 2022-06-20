2136450a835a6af503db0de9b45e35778bd59f6a
mudigone <76899523+mudigone@users.noreply.github.com>
2022-06-20 17:58:35 +0500

    Final Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x03268dd390955964dd47F623c244c744a32eAA7a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
